45e4077f180295e719937af25e98ba43e458d9f5
Gregor Zurowski <gregor@zurowski.org>
2023-11-02 11:20:53 +0100

    Use Github hosted repos

--- public/clone-camel.sh
+++ public/clone-camel.sh
@@ -6,7 +6,7 @@ if [ -d "${REPO_ROOT}/camel" ] && [ -d "${REPO_ROOT}/camel-karaf" ] && [ -d "${R
     echo "Camel repositories already present"
 else
     mkdir -p ${REPO_ROOT}/camel ${REPO_ROOT}/camel-karaf ${REPO_ROOT}/camel-spring-boot
-    git clone https://gitbox.apache.org/repos/asf/camel.git ${REPO_ROOT}/camel
-    git clone https://gitbox.apache.org/repos/asf/camel-karaf.git ${REPO_ROOT}/camel-karaf && \
-    git clone https://gitbox.apache.org/repos/asf/camel-spring-boot.git ${REPO_ROOT}/camel-spring-boot
+    git clone https://github.com/apache/camel.git ${REPO_ROOT}/camel
+    git clone https://github.com/apache/camel-karaf.git ${REPO_ROOT}/camel-karaf && \
+    git clone https://github.com/apache/camel-spring-boot.git ${REPO_ROOT}/camel-spring-boot
 fi
